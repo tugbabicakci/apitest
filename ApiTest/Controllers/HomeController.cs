b22f692a1abd023aae862d189a4f48a9bc6a3252
tugba.bicakci <tugba.bicakci@GTBICAKCIW10>
2018-08-02 10:32:30 +0300

    Add project files.

---
+++ ApiTest/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiTest.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using ApiTest.Helper;
+
+namespace ApiTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        CostumerApi _api = new CostumerApi();
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            List<Costumer> list = new List<Costumer>();
+            HttpClient client = _api.Inıtal();
+            HttpResponseMessage res = await client.GetAsync("/api/costumer/");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                list = JsonConvert.DeserializeObject<List<Costumer>>(result);
+
+
+            }
+            return View(list);
+
+
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Costumer obj)
+        {
+            HttpClient client = _api.Inıtal();
+            string stringData = JsonConvert.SerializeObject(obj);
+            var contentData = new StringContent
+        (stringData, System.Text.Encoding.UTF8,
+        "application/json");
+            HttpResponseMessage response = client.PostAsync
+        ("/api/costumer", contentData).Result;
+            ViewBag.Message = response.Content.
+        ReadAsStringAsync().Result;
+
+
+
+            return View(obj);
+
+        }
+
+
+        public IActionResult Update(string id)
+        {
+            
+          
+
+            HttpClient client = _api.Inıtal();
+            HttpResponseMessage response =
+           client.GetAsync("/api/costumer/" + id).Result;
+            string stringData = response.Content.
+        ReadAsStringAsync().Result;
+            Costumer data = JsonConvert.
+        DeserializeObject<Costumer>(stringData);
+            return View(data);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Costumer obj)
+        {
+
+            HttpClient client = _api.Inıtal();
+            string stringData = JsonConvert.SerializeObject(obj);
+            var contentData = new StringContent(stringData,
+        System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync
+        ("/api/costumer/" + obj.Id,
+        contentData).Result;
+            ViewBag.Message = response.Content.
+        ReadAsStringAsync().Result;
+            return View(obj);
+        }
+
+
+     
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            HttpClient client = _api.Inıtal();
+            HttpResponseMessage response = client.DeleteAsync("/api/costumer/" + id).Result;
+            TempData["Message"] =
+        response.Content.ReadAsStringAsync().Result;
+            return RedirectToAction("Index");
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+}
---
+++ ApiTest/Helper/CostumerApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ApiTest.Helper
+{
+    public class CostumerApi
+    {
+        public HttpClient Inıtal()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44381/");
+            MediaTypeWithQualityHeaderValue contentType =new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+
+            return client;
+        }
+    }
+}
